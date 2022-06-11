8eaceb46b3e04a961ce67d5effa15475605f24f6
admin <catsatan>
2022-06-11 16:25:07 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientOc = 0
+                if (clientOc == 0)
+                {
+                    System.out.println("установите версию для Android");
+                }
+                else {
+                    System.out.println("установите версию для IOS");
+                }
+    }
+}
